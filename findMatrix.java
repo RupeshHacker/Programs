a4d1a806b13f371016e400f08e6bdb3f70e6bb75
RupeshHacker <rupeshpowar1234@gmail.com>
2024-02-27 22:32:04 +0530

    Added Matrix

---
+++ findMatrix.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * findMatrix
+ */
+public class findMatrix {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,1,2,3,1};
+        findMatrix fm = new findMatrix();
+        fm.findMatrix(nums);
+    }
+}
